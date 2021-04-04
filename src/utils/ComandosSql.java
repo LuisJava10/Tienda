aad182c8522d5ccc9f4295bdd6d0573eab27433d
samuel <samuelbulla08@gmail.com>
2021-04-04 16:29:23 -0500

    Se agregaron los comandos sql

---
+++ src/utils/ComandosSql.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ComandosSql {
+    public static final int ARTICULO_CREAR = 110;
+    public static final int ARTICULO_CONSULTAR_POR_ID = 120;
+    public static final int ARTICULO_CONSULTAR_TODOS = 121;
+    public static final int ARTICULO_ACTUALIZAR_POR_ID = 130;
+    public static final int ARTICULO_ELIMINAR_POR_ID = 140;
+    
+    public static final int SERVICIO_CREAR = 210;
+    public static final int SERVICIO_CONSULTAR_POR_ID = 220;
+    public static final int SERVICIO_CONSULTAR_TODOS = 221;
+    public static final int SERVICIO_ACTUALIZAR_POR_ID = 230;
+    public static final int SERVICIO_ELIMINAR_POR_ID = 240;
+    
+}
